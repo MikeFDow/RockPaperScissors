0929a6761e97495359c5d50f664c30f09dfd961b
SalmonBoyFred <michaelfdow025@gmail.com>
2022-04-14 01:57:37 -0400

    user interface

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -24,7 +24,7 @@ function computerPlay () {
 function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay();
     if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "YOU WIN";
+        return( "YOU WIN");
     } else if (playerSelection === "scissors" && computerSelection === "paper" ) {
         return "YOU WIN";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
@@ -52,17 +52,17 @@ let comScore = 0;
 
 function game() {
     if (playerScore < 5 && comScore < 5) {
-        let playerSelection = prompt("?").toLowerCase();
+        // let playerSelection = prompt("?").toLowerCase();
         let a = playRound(playerSelection, computerSelection);
         switch (a) {
             case "YOU WIN":
-                console.log(++playerScore);
-                return `You win! Your score is ${playerScore}`;
+                ++pScore;
+                return; //`You win! Your score is ${playerScore}`;
                 break;
             case "You lose":
-                console.log(++comScore);
+                ++comScore;
                 return `You lose. Computers score is ${comScore}`;
-            break;
+                break;
             case "Tie":
                 return "Tie, try again";
                 break;
@@ -165,3 +165,129 @@ function getRandomInteger (min, max) {
     max = Math.floor(4);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+const container = document.querySelector('#container'); // div for title
+
+
+const title = document.createElement('h1');
+title.textContent = 'Rock | Paper | Scissors';
+
+container.appendChild(title);
+
+
+const results = document.querySelector('#results');  // div for results
+
+//let pScore = document.createElement('p');                // element for players score
+//pScore.textContent = `player score is ${playerScore}`;
+
+//results.appendChild(pScore);
+
+//const pScore = document.querySelector('#pScore'); // one method for trying to display score
+//let pNumber = playerScore;
+//pScore.textContent = parseInt(pNumber);
+
+//const cScore = document.createElement('p');     // element for computers score
+//cScore.textContent = `Computers score is ${comScore}`;
+
+//results.appendChild(cScore);
+
+/* const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert(button.id);
+        
+    });
+});
+
+*/
+
+// EVENT LISTENER AND FUNCTION FOR ROCK BUTTON
+
+const rockButton = document.querySelector('#rockBtn'); // THIS WORKS!!! :)
+rockButton.addEventListener('click', () => {
+    let result = playRound('rock');
+    document.querySelector('#message').textContent = result;
+    if (result == "YOU WIN") {
+        ++playerScore;
+    } else if (result == "You lose") {
+        ++comScore;
+    } else if (result == "Tie") {
+        return;
+    }
+    document.querySelector('#pScore').textContent = `Player: ${playerScore}`;
+    document.querySelector('#cScore').textContent =  `Computer: ${comScore}`;
+    endGame();
+});
+
+// EVENT LISTENER AND FUNCTION FOR PAPER BUTTON
+
+const paperButton = document.querySelector('#paperBtn');
+paperButton.addEventListener('click', () => {
+    let result = playRound('paper');
+    //alert(result);
+    document.querySelector('#message').textContent = result;
+    if (result == "YOU WIN") {
+        ++playerScore;
+    } else if (result == "You lose") {
+        ++comScore;
+    } else if (result == "Tie") {
+        return;
+    }
+    document.querySelector('#pScore').textContent = `Player: ${playerScore}`;
+    document.querySelector('#cScore').textContent =  `Computer: ${comScore}`;
+    endGame();
+});
+
+// EVENT LISTENER AND FUNCTION FOR SCISSORS BUTTON
+
+const scissorsButton = document.querySelector('#scissorsBtn');
+scissorsButton.addEventListener('click', () => {
+    let result = playRound('scissors');
+    document.querySelector('#message').textContent = result;
+    //alert(result);
+    if (result == "YOU WIN") {
+        ++playerScore;
+    } else if (result == "You lose") {
+        ++comScore;
+    } else if (result == "Tie") {
+        return;
+    }
+    document.querySelector('#pScore').textContent = `Player: ${playerScore}`;
+    document.querySelector('#cScore').textContent =  `Computer: ${comScore}`;
+    endGame();
+});
+
+// END GAME FUNCTION
+
+function endGame() {
+    if (playerScore === 5) {
+        //alert("Game Over, You win!");
+        document.querySelector('#message').textContent = "Game over, You win!";
+        playerScore = 0;
+        comScore = 0;
+    } else if (comScore === 5) {
+        //alert("Game Over, You lose!");
+        document.querySelector('#message').textContent = "Game over, You lose!";
+        playerScore = 0;
+        comScore = 0;
+    } else {
+        return;
+    }
+};
+
+
+
+/*
+const rockButton = document.querySelector('#rockBtn'); // THIS WORKS!!! :)
+rockButton.addEventListener('click', () => {
+    playerSelection = 'rock';
+    game();
+
+});
+*/
+
+// https://stackoverflow.com/questions/61159599/rock-paper-scissors-output-is-inconsistent-with-the-result
+
+
+
+// NEW GAME 
